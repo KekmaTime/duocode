2563f94c6598c11d1b3f8e8c0a6c15e4ab82ec3b
KekmaTime <22am014@sctce.ac.in>
2024-07-22 01:20:48 +0530

    feat(ui): add avatar and card components; enhance header and layout

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,19 +2,72 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { LogInIcon, LogOutIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+
+
+function AccountDropDown(){
+  const session = useSession();
+  const isLoggedIn = !!session.data;
+
+  return(
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="link">
+          <Avatar>
+            <AvatarImage src={session.data?.user?.image || ""} />
+            <AvatarFallback>{session.data?.user?.name}</AvatarFallback>
+          </Avatar>
+          {session.data?.user?.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {isLoggedIn ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+              <LogOutIcon className="w-4 h-4 mr-2"/>Log out
+            </DropdownMenuItem>
+          ) : (
+            <DropdownMenuItem onClick={() => signIn("google")}>
+              <LogInIcon className="w-4 h-4 mr-2"/>Log in
+            </DropdownMenuItem>
+          )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
 
 export function Header() {
   const session = useSession();
+  const { theme } = useTheme();
+  const isDarkMode = theme === "dark";
 
   return (
-    <div className="flex justify-end">
-      {session.data ? (
-        <Button onClick={() => signOut()}>Sign out</Button>
-      ) : (
-        <Button onClick={() => signIn("google")}>Sign in</Button>
-      )}
-      { session.data?.user?.name }
-      <ModeToggle />
-    </div>
+    <header className="py-2 px-2 mx-auto">
+      <div className="flex justify-between items-center">
+          <Link href="/" className="flex gap-2 items-center text-xl hover:underline">
+          <Image 
+            src="/logo.png" 
+            alt="Duocode Icon"
+            width="60"
+            height="60"
+            className={isDarkMode ? "filter invert" : ""}
+          />
+          DuoCode
+          </Link>
+        <div className="flex items-center gap-4">
+          <AccountDropDown />
+          <ModeToggle />
+        </div>
+      </div>
+    </header>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,48 @@
+import { Button } from "@/components/ui/button";
 import { db } from "@/db";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Room } from "@/db/schema";
+
+function RoomCard({room}: {room: Room}){
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{room.name}</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+  </Card>
+  );
+}
+
 export default async function Home() {
 
-  const item = await db.query.room.findMany();
+  const rooms = await db.query.room.findMany();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {
-        item.map((room) => {
-          return <div key={room.id}>{room.name}</div>;
-        })
+    <main className="min-h-screen p-8">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">Find Rooms to Code</h1>
+        <Button asChild>
+          <Link href="/create-room">Create Room</Link>
+        </Button>
+      </div>
+      {rooms.map((room) => { 
+        return <RoomCard key={room.id} room={room} />;
       }
+    )}
     </main>
   );
 }
